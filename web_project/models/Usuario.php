91d3a3bbce9ecf758d1c9fb7a1f6dfdf6b880b1e
1michael4 <Mdavidramado@gmail.com>
2020-11-04 23:33:14 -0500

    arreglos 2.0

--- web_project/models/Usuario.php
+++ web_project/models/Usuario.php
@@ -4,7 +4,7 @@ class Usuario extends DB{
 
     public function request(){
 		try{
-			$stm=parent::connect()->prepare("SELECT * FROM usuario INNER JOIN rol ON usuario.fk_rol = rol.id_rol");
+			$stm=parent::connect()->prepare("SELECT * FROM usuario INNER JOIN rol ON usuario.fk_rol = rol.id_rol INNER JOIN tipo_documento ON usuario.fk_tipo_documento = tipo_documento.id_tipo_documento");
 			$stm->execute();
 			return $stm->fetchALL(PDO::FETCH_OBJ);
 		}catch(Exception $e){
