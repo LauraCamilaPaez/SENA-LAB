5b65cb95378bfd93e105dbc5c260573ecbfdcb1a
diegorojas0510 <40213952+diegorojas0510@users.noreply.github.com>
2020-11-08 15:46:51 -0500

    base de datos

--- database/sena-lab.sql
+++ database/sena-lab.sql
@@ -2,8 +2,8 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 05-11-2020 a las 05:34:08
+-- Servidor: localhost:3306
+-- Tiempo de generación: 08-11-2020 a las 19:44:10
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -20,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `sena-lab`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `incapacidades`
+--
+
+CREATE TABLE `incapacidades` (
+  `id_incapacidades` int(11) NOT NULL,
+  `nombre` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telefono` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `correo` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mensaje` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pdf` varchar(125) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `incapacidades`
+--
+
+INSERT INTO `incapacidades` (`id_incapacidades`, `nombre`, `telefono`, `correo`, `mensaje`, `pdf`) VALUES
+(1, 'juan', '3057119159', 'jsdbhj@hejef.com', 'fsjhsukhjsk', 'assets/archivos/'),
+(2, 'juan', '3057119159', 'jsdbhj@hejef.com', 'fsjhsukhjsk', 'assets/archivos/'),
+(3, '', '', '', '', 'assets/archivos/'),
+(4, 'juan', '3057119159', 'jsdbhj@hejef.com', 'sfsdf', 'assets/archivos/'),
+(5, '', '', '', '', 'assets/archivos/'),
+(6, 'diego', '3144237880', 'diegorojas.0510@gmail.com', 'hola', 'assets/archivos/'),
+(7, '', '', 'diegorojas.0510@gmail.com', '', 'assets/archivos/'),
+(8, '', '', 'diegorojas.0510@gmail.com', '', 'assets/archivos/'),
+(9, '', '', 'd@gmail.com', '', 'assets/archivos/'),
+(10, '', '', 'fla****rma***oj****otmail.com', '', 'assets/archivos/'),
+(11, '', '', 'diegorojas.0510@gmail.com', '', 'assets/archivos/');
 
 -- --------------------------------------------------------
 
@@ -91,6 +123,7 @@ CREATE TABLE `usuario` (
   `fk_tipo_documento` int(11) NOT NULL,
   `fk_tipo_contrato` int(11) NOT NULL,
   `fk_rol` int(11) NOT NULL,
+  `fk_incapacidades` int(11) NOT NULL,
   `nombre` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `apellido` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `correo` text COLLATE utf8_unicode_ci NOT NULL,
@@ -104,13 +137,20 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `fk_tipo_documento`, `fk_tipo_contrato`, `fk_rol`, `nombre`, `apellido`, `correo`, `documento`, `password_user`, `estado`, `foto`) VALUES
-(1, 1, 3, 2, 'prueba prueba1', 'prueba prueba1', 'prueba@gmail.com', 1001256482, '12345', 'activo', ''),
-(3, 1, 3, 3, 'prueba prueba2', 'prueba prueba2', 'J@gmail.com', 52363833, '12345', '', '');
+INSERT INTO `usuario` (`id_usuario`, `fk_tipo_documento`, `fk_tipo_contrato`, `fk_rol`, `fk_incapacidades`, `nombre`, `apellido`, `correo`, `documento`, `password_user`, `estado`, `foto`) VALUES
+(5, 1, 2, 2, 0, 'diego', 'rojas', 'd@gmail.com', 1032369406, '12345', 'activo', ''),
+(13, 1, 1, 1, 0, 'juan', 'rojas', 'j@gmail.com', 123456, '123', 'activo', ''),
+(14, 1, 2, 3, 0, 'kevin', 'rojas', 'k@gmail.com', 1234565678, '123', 'activo', '');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `incapacidades`
+--
+ALTER TABLE `incapacidades`
+  ADD PRIMARY KEY (`id_incapacidades`);
 
 --
 -- Indices de la tabla `rol`
@@ -144,6 +184,12 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `incapacidades`
+--
+ALTER TABLE `incapacidades`
+  MODIFY `id_incapacidades` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
@@ -165,7 +211,7 @@ ALTER TABLE `tipo_documento`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
