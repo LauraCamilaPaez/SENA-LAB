cad409f656c5bf9a43f8c50f2b339d42f24ade0d
1michael4 <Mdavidramado@gmail.com>
2020-11-22 15:30:07 -0500

    incapacidades

--- web_project/views/incapacidades/envioIncapacidades.php
+++ web_project/views/incapacidades/envioIncapacidades.php
@@ -30,34 +30,22 @@
         <div class="col-md-8  offset-md-4">
             <form action="?c=Incapacidades&m=create" method = "POST" enctype = "multipart/form-data" class="form-incapacidades">
                 <div class="form-group">
+                    <label class="label-incapacidades" for="form">nombre</label>
+                    <input type="text" name="nombre" class="form-control" placeholder="Introduce tu nombre" required>
+                    <label class="label-incapacidades" for="form">Telefono</label>
+                    <input type="number" name="telefono" class="form-control" placeholder="Introduce tu telefono" required>
                     <label class="label-incapacidades" for="form">Correo</label>
                     <input type="correo" name="correo" class="form-control" placeholder="Introduce tu correo" required>
                     <label class="label-incapacidades" for="form">Descripción incapacidad</label>
-                    <input type="text" name="descripcion" class="form-control" placeholder="Motivo incapacidad" required>
+                    <input type="text" name="mensaje" class="form-control" placeholder="Motivo incapacidad" required>
                     <label class="label-incapacidades" for="form">Agregar Incapacidad</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    <input type="file" class="form-control-file" name="pdf" id="exampleFormControlFile1">
                     <button type="submit" class="btn-primary">Enviar</button>
                 </div>
             </form>
         </div>
     </header>
-    <div class="container-incapacidades">
-        <div class="row">
-            <div class="col-md-8  offset-md-4">
-                <form action="?c=Incapacidades&m=create" method="POST" enctype="multipart/form-data" class="form-incapacidades">
-                    <div class="form-group">
-                        <label class="label-incapacidades" for="form">Correo</label>
-                        <input type="correo" name="correo" class="form-control" placeholder="Introduce tu correo" required>
-                        <label class="label-incapacidades" for="form">Descripción incapacidad</label>
-                        <input type="text" name="descripcion" class="form-control" placeholder="Motivo incapacidad" required>
-                        <label class="label-incapacidades" for="form">Agregar Incapacidad</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                        <button type="submit" class="btn-primary">Enviar</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+   
 
     <footer class="text-center p-3 mb-2 bg-light text-white bg-dark">
         <p>Todos los derechos reservados a Sena-lab</p>
